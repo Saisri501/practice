9d169c1f19a4b15a725188b1fd33c28ba5b91479
Saisri501 <98950412+Saisri501@users.noreply.github.com>
2022-02-08 18:28:15 +0530

    Update main.tf

--- .github/workflows/modules/ResourceGroup/main.tf
+++ .github/workflows/modules/ResourceGroup/main.tf
@@ -1,3 +1,11 @@
+terraform {
+    backend "azurerm" {
+        resource_group_name ="resourcegroup1"
+        storage_account_name ="storage5011"
+        container_name="container"
+        key="dev.terraform.tfstate"
+    }
+}        
 resource "azurerm_resource_group" "main" {
     name        = var.resource_group
     location    = var.location 
